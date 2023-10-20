38974c9dd217ac8e43a86066317a59c45e7a63e2
Dimitar Tsvetkov <dimitartsvetkov@MacBook-Pro-von-Dimitar.local>
2023-10-20 17:18:05 +0300

    [us-3]-adding-new-expenses-functionality

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
+final formatter = DateFormat.yMd();
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
+};
 
 class Expense {
   // generate unique uuid when the class is initialised by using Dart 'Initializer'
@@ -18,4 +28,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,13 @@ class Expenses extends StatefulWidget {
 }
 
 class _Expenses extends State<Expenses> {
+  void _openAddExpensesOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: addExpense),
+    );
+  }
+
   final List<Expense> _registeredExpenses = [
     Expense(
         title: 'Flutter Course',
@@ -23,9 +31,21 @@ class _Expenses extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Personal Expense Tracker'), actions: [
+        IconButton(
+          onPressed: _openAddExpensesOverlay,
+          icon: const Icon(Icons.add),
+        ),
+      ]),
       body: Column(
         children: [
           const Text('Chart'),
