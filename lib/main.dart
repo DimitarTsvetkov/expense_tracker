05fd30bb0df7be72ee02371c5642a7977a61a5c7
Dimitar Tsvetkov <dimitartsvetkov@MacBook-Pro-von-Dimitar.local>
2023-10-25 00:24:00 +0300

    [us-6]-locking-to-portraitUp-mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/entry_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // import 'package:expense_tracker/widgets/expenses.dart';
 
@@ -12,44 +13,49 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-          useMaterial3: true,
-          colorScheme: kDarkColorScheme,
-          cardTheme: const CardTheme().copyWith(
-            color: kDarkColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: kDarkColorScheme.primaryContainer,
-                foregroundColor: kDarkColorScheme.onPrimaryContainer),
-          )),
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        // scaffoldBackgroundColor: Color.fromARGB(255, 64, 35, 143),
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: const Color.fromARGB(255, 64, 35, 143),
-            foregroundColor: kColorScheme.primaryContainer),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: kColorScheme.onSecondaryContainer,
-                  fontSize: 18),
+  //lock only to portraitUp mode
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  ).then((fn) => runApp(
+        MaterialApp(
+          darkTheme: ThemeData.dark().copyWith(
+            useMaterial3: true,
+            colorScheme: kDarkColorScheme,
+            cardTheme: const CardTheme().copyWith(
+              color: kDarkColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             ),
-      ),
-      themeMode: ThemeMode.system, // default
-      home: const EntryScreen(),
-    ),
-  );
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kDarkColorScheme.primaryContainer,
+                  foregroundColor: kDarkColorScheme.onPrimaryContainer),
+            ),
+          ),
+          theme: ThemeData().copyWith(
+            useMaterial3: true,
+            // scaffoldBackgroundColor: Color.fromARGB(255, 64, 35, 143),
+            colorScheme: kColorScheme,
+            appBarTheme: const AppBarTheme().copyWith(
+                backgroundColor: const Color.fromARGB(255, 64, 35, 143),
+                foregroundColor: kColorScheme.primaryContainer),
+            cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kColorScheme.primaryContainer),
+            ),
+            textTheme: ThemeData().textTheme.copyWith(
+                  titleLarge: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: kColorScheme.onSecondaryContainer,
+                      fontSize: 18),
+                ),
+          ),
+          themeMode: ThemeMode.system, // default
+          home: const EntryScreen(),
+        ),
+      ));
 }
